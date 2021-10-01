b27e95336fcbae0a9ce05831b10cd79dfdc67354
IAmNotAProgramer <dawidsce@gmail.com>
2021-10-01 09:36:01 +0200

    GPIO: adjust first modules to work with STM32F4

--- Archive/GPIO_4/4.3.c
+++ Archive/GPIO_4/4.3.c
@@ -1,10 +1,21 @@
-#include<LPC21xx.H>
+
+#define LED_GREEN_SET  (1 << 12)
+#define LED_GREEN_DIR  (1 << 24)
+
+#define GPIOD_EN       (1 << 3)
+
+#define RCCAHB1   (*((volatile unsigned long *) 0x40023830)) // uint32_t *pRccAhb1enr = (uint32_t*)0x40023830;
+#define GPIODMODE (*((volatile unsigned long *) 0x40020C00)) // uint32_t *pGpiodModeReg = (uint32_t*)0x40020C00;
+#define GPIODDATA (*((volatile unsigned long *) 0x40020C14)) // uint32_t *pGpiodDataReg = (uint32_t*)0x40020C14;
 
 int main(){
-	IO1DIR=0x00010000;
+	// Enable clock for peripherals
+	RCCAHB1 |= GPIOD_EN;
+	// configure led as an output
+	GPIODMODE |= LED_GREEN_DIR;
 	
 	while(1){
-		IO1SET=0x00010000;
-		IO1CLR=0x00010000;
+		GPIODDATA |= LED_GREEN_SET;
+		GPIODDATA &= ~LED_GREEN_SET;
 	}
 }
